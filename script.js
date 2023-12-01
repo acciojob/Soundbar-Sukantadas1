c4129411b7cd060ee639656762c01055b9267eeb
Sukanta Das <111263085+Sukantadas1@users.noreply.github.com>
2023-12-01 19:52:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let audio;
+
+function playAudio(soundFileName) {
+    if (audio) {
+        stopAudio();
+    }
+
+    audio = new Audio(`sounds/${soundFileName}`);
+    audio.play();
+}
+
+function stopAudio() {
+    if (audio) {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+}
